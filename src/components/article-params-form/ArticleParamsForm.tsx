bb893183f0cb6edb18c5d5b348cc0f9ed42b5970
Danila Niurkin <dnurkin@gmail.com>
2025-06-11 20:00:35 +0300

    Реализовано открытие/закрытие формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,21 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState } from "react";
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
+type FormState = 'closed' | 'opened';
+
 export const ArticleParamsForm = () => {
+	const [state, setState] = useState<FormState>('closed');
+	const toggleState = () => {
+		setState(state === 'closed' ? 'opened' : 'closed');
+	};
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={state === 'closed' ? false : true} onClick={toggleState} />
+			<aside className={clsx(styles.container, { [styles.container_open]: state === 'opened' })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
