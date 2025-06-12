84e8669e0cc1ba15a14183d055882c6f6c99b57b
Danila Niurkin <dnurkin@gmail.com>
2025-06-12 18:25:04 +0300

    Добавлено закрытие формы по клику вне формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,6 +1,6 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import { useState } from "react";
+import { useState, useRef, useEffect } from 'react';
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
@@ -9,13 +9,35 @@ type FormState = 'closed' | 'opened';
 
 export const ArticleParamsForm = () => {
 	const [state, setState] = useState<FormState>('closed');
+	const containerRef = useRef<HTMLDivElement | null>(null);
+
 	const toggleState = () => {
-		setState(state === 'closed' ? 'opened' : 'closed');
+		setState((prev) => (prev === 'closed' ? 'opened' : 'closed'));
 	};
+
+	useEffect(() => {
+		const handleClickOutside = (evt: MouseEvent) => {
+			if (
+				state === 'opened' &&
+				containerRef.current &&
+				!containerRef.current.contains(evt.target as Node)
+			) {
+				setState('closed');
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [state]);
+
 	return (
 		<>
-			<ArrowButton isOpen={state === 'closed' ? false : true} onClick={toggleState} />
-			<aside className={clsx(styles.container, { [styles.container_open]: state === 'opened' })}>
+			<ArrowButton isOpen={state === 'opened'} onClick={toggleState} />
+			<aside
+			  ref={containerRef}
+			  className={clsx(styles.container, { [styles.container_open]: state === 'opened' })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
